0a7e8af30ee20ebd4763ceb437eeaa43006a7a00
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2014-10-04 02:51:17 -0700

    layout is in progress...

--- app/src/main/java/com/calhacksproject/teamfunkybeefwrapthings/movietrak/Main.java
+++ app/src/main/java/com/calhacksproject/teamfunkybeefwrapthings/movietrak/Main.java
@@ -1,9 +1,13 @@
 package com.calhacksproject.teamfunkybeefwrapthings.movietrak;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class Main extends Activity {
@@ -12,9 +16,27 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeLists();
+
     }
 
+    private void initializeLists(){
+        Dialog dialog = new Dialog(this);
+        AlertDialog.Builder builder;
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Color Mode");
 
+        ListView modeList = new ListView(this);
+        String[] stringArray = new String[] { "Bright Mode", "Normal Mode" };
+        ArrayAdapter<String> modeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, stringArray);
+        modeList.setAdapter(modeAdapter);
+
+        builder.setView(modeList);
+        dialog = builder.create();
+    }
+
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -33,4 +55,5 @@ public class Main extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    */
 }
